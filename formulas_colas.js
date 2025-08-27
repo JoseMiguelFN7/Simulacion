6ede3fe00ac8cd03195b648bf36f964ac45f7357
JoseMiguelFN7 <jferreira.5655@unimar.edu.ve>
2025-08-26 23:26:57 -0400

    Funcion para N servidores con cola limitada

--- formulas_colas.js
+++ formulas_colas.js
@@ -1,5 +1,5 @@
 function redondear(num, decimales) {
-    let factor = Math.pow(10, decimales);
+    const factor = Math.pow(10, decimales);
     return Math.round(num * factor) / factor;
 }
 
@@ -16,12 +16,12 @@ function factorial(n) {
 function getColaNoLimite1Servidor(lambda, mu, k){
     let results = [];
 
-    let rho = lambda/mu;
-    let P0 = 1 - rho;
-    let Ls = rho / (1 - rho);
-    let Lq = (rho * rho) / (1 - rho);
-    let Ws = Ls/lambda;
-    let Wq = Lq/lambda;
+    const rho = lambda/mu;
+    const P0 = 1 - rho;
+    const Ls = rho / (1 - rho);
+    const Lq = (rho * rho) / (1 - rho);
+    const Ws = Ls/lambda;
+    const Wq = Lq/lambda;
 
     results.push({name: "rho", value: redondear(rho, 6)});
     results.push({name: "Wq", value: redondear(Wq, 6)});
@@ -44,18 +44,18 @@ function getColaNoLimite1Servidor(lambda, mu, k){
 }
 
 function getColaLimite1Servidor(lambda, mu, cola){
-    let N = cola+1
+    const N = cola+1
     let results = [];
     
-    let rho = lambda/mu;
-    let P0 = (1-rho)/(1-rho**(N+1));
-    let Ls = (rho * (1 - (N + 1) * rho**N + N * rho**(N + 1))) / ((1 - rho) * (1 - rho**(N + 1)));
-    let PN = P0*rho**N;
-    let lambdaEfect = lambda*(1-PN);
-    let lambdaPerd = lambda-lambdaEfect;
-    let Lq = Ls-lambdaEfect/mu;
-    let Wq = Lq/lambdaEfect;
-    let Ws = Wq+1/mu;
+    const rho = lambda/mu;
+    const P0 = (1-rho)/(1-rho**(N+1));
+    const Ls = (rho * (1 - (N + 1) * rho**N + N * rho**(N + 1))) / ((1 - rho) * (1 - rho**(N + 1)));
+    const PN = P0*rho**N;
+    const lambdaEfect = lambda*(1-PN);
+    const lambdaPerd = lambda-lambdaEfect;
+    const Lq = Ls-lambdaEfect/mu;
+    const Wq = Lq/lambdaEfect;
+    const Ws = Wq+1/mu;
 
     results.push({name: "rho", value: redondear(rho, 6)});
     results.push({name: "Wq", value: redondear(Wq, 6)});
@@ -82,7 +82,7 @@ function getColaLimite1Servidor(lambda, mu, cola){
 function getColaNoLimiteNServidor(lambda, mu, C, k){
     results = [];
 
-    let rho = lambda/mu;
+    const rho = lambda/mu;
     let P0 = 0;
     for (i = 0; i <= C; i++){
         if(i == C){
@@ -94,12 +94,11 @@ function getColaNoLimiteNServidor(lambda, mu, C, k){
         }
     }
     P0 = 1/P0;
-    console.log(P0);
-    let PC = rho**C/factorial(C)*P0;
-    let Lq = (rho*C*PC)/(C-rho)**2
-    let Ls = rho+Lq;
-    let Wq = Lq/lambda;
-    let Ws = Wq+1/mu;
+    const PC = rho**C/factorial(C)*P0;
+    const Lq = (rho*C*PC)/(C-rho)**2
+    const Ls = rho+Lq;
+    const Wq = Lq/lambda;
+    const Ws = Wq+1/mu;
 
     results.push({name: "rho", value: redondear(rho, 6)});
     results.push({name: "Wq", value: redondear(Wq, 6)});
@@ -128,4 +127,65 @@ function getColaNoLimiteNServidor(lambda, mu, C, k){
     return results;
 }
 
-console.log(JSON.stringify(getColaNoLimiteNServidor(0.25, 0.1, 3, 60), null, 4));
+function getColaLimiteNServidor(lambda, mu, C, cola){
+    results = [];
+
+    const N = C + cola;
+    const rho = lambda/mu;
+    const rhoC = rho/C;
+    let P0 = 0;
+    for (i = 0; i <= C; i++){
+        if(i == C){
+            P0 += (rho**C*(1-rhoC**(N-C+1)))/(factorial(C)*(1-rhoC));
+        }
+        else
+        {
+            P0 += rho**i/factorial(i);
+        }
+    }
+    P0 = 1/P0;
+    const PN = (rho**N*P0)/(C**(N-C)*factorial(C));
+    const lambdaEfect = lambda*(1-PN);
+    const lambdaPerd = lambda-lambdaEfect;
+    const Lq = ((P0*rho**(C+1))/(factorial(C-1)*(C-rho)**2))*(1-rhoC**(N-C)-(N-C)*rhoC**(N-C)*(1-rhoC));
+    const Ls = Lq+lambdaEfect/mu;
+    const Wq = Lq/lambdaEfect;
+    const Ws = Wq+1/mu;
+    let cnPn = 0;
+
+    distribution = [];
+    let Fn = 0;
+    for(let n = 0; n <= N; n++){
+        let Pn = 0;
+        if(n<=C){
+            Pn = rho**n/factorial(n)*P0;
+            cnPn += (C-n)*Pn;
+        }
+        else
+        {
+            Pn = (rho**n*P0)/(C**(n-C)*factorial(C));
+        }
+        let PnRounded = redondear(Pn, 6); // Guardar el valor redondeado de Pn
+        Fn += Pn;
+        let FnRounded = redondear(Fn, 6); // Guardar el valor redondeado de Fn
+
+        distribution.push({n: n, Pn: PnRounded, Fn: FnRounded});
+    }
+    results.push({name: "distribution", value: distribution});
+
+    const Cactivos = C-cnPn;
+
+    results.push({name: "rho", value: redondear(rho, 6)});
+    results.push({name: "Wq", value: redondear(Wq, 6)});
+    results.push({name: "Ws", value: redondear(Ws, 6)});
+    results.push({name: "Lq", value: redondear(Lq, 6)});
+    results.push({name: "Ls", value: redondear(Ls, 6)});
+    results.push({name: "lambdaEfect", value: redondear(lambdaEfect, 6)});
+    results.push({name: "lambdaPerd", value: redondear(lambdaPerd, 6)});
+    results.push({name: "Cactivos", value: redondear(Cactivos, 6)});
+    results.push({name: "Cinactivos", value: redondear(cnPn, 6)});
+
+    return results;
+}
+
+console.log(JSON.stringify(getColaLimiteNServidor(0.25, 0.1, 3, 13), null, 4));
