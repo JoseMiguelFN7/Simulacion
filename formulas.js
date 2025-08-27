3071c4b4c004d995ef7eccb52be0712d66ea6f08
JoseMiguelFN7 <jferreira.5655@unimar.edu.ve>
2025-08-26 20:50:53 -0400

    funcion de cola N servidores sin limite

--- formulas.js
+++ formulas.js
@@ -3,21 +3,31 @@ function redondear(num, decimales) {
     return Math.round(num * factor) / factor;
 }
 
+function factorial(n) {
+    if (n < 0) return NaN;
+    if (n === 0 || n === 1) return 1;
+        let result = 1;
+    for (let i = 2; i <= n; i++) {
+        result *= i;
+    }
+    return result;
+}
+
 function getColaNoLimite1Servidor(lambda, mu, k){
     let results = [];
 
-    let rho = redondear(lambda/mu, 6);
-    let P0 = redondear(1 - rho, 6);
-    let Ls = redondear(rho / (1 - rho), 6);
-    let Lq = redondear((rho * rho) / (1 - rho), 6);
-    let Ws = redondear(Ls/lambda, 6);
-    let Wq = redondear(Lq/lambda, 6);
+    let rho = lambda/mu;
+    let P0 = 1 - rho;
+    let Ls = rho / (1 - rho);
+    let Lq = (rho * rho) / (1 - rho);
+    let Ws = Ls/lambda;
+    let Wq = Lq/lambda;
 
-    results.push({name: "rho", value: rho});
-    results.push({name: "Wq", value: Wq});
-    results.push({name: "Ws", value: Ws});
-    results.push({name: "Lq", value: Lq});
-    results.push({name: "Ls", value: Ls});
+    results.push({name: "rho", value: redondear(rho, 6)});
+    results.push({name: "Wq", value: redondear(Wq, 6)});
+    results.push({name: "Ws", value: redondear(Ws, 6)});
+    results.push({name: "Lq", value: redondear(Lq, 6)});
+    results.push({name: "Ls", value: redondear(Ls, 6)});
 
     distribution = [];
     let Fn = 0;
@@ -69,5 +79,53 @@ function getColaLimite1Servidor(lambda, mu, cola){
     return results;
 }
 
-console.log(JSON.stringify(getColaNoLimite1Servidor(0.125, 1/6, 20), null, 4));
-console.log(JSON.stringify(getColaLimite1Servidor(0.125, 1/6, 5), null, 4));
+function getColaNoLimiteNServidor(lambda, mu, C, k){
+    results = [];
+
+    let rho = lambda/mu;
+    let P0 = 0;
+    for (i = 0; i <= C; i++){
+        if(i == C){
+            P0 += rho**C/(factorial(C)*(1-rho/C));
+        }
+        else
+        {
+            P0 += rho**i/factorial(i);
+        }
+    }
+    P0 = 1/P0;
+    console.log(P0);
+    let PC = rho**C/factorial(C)*P0;
+    let Lq = (rho*C*PC)/(C-rho)**2
+    let Ls = rho+Lq;
+    let Wq = Lq/lambda;
+    let Ws = Wq+1/mu;
+
+    results.push({name: "rho", value: redondear(rho, 6)});
+    results.push({name: "Wq", value: redondear(Wq, 6)});
+    results.push({name: "Ws", value: redondear(Ws, 6)});
+    results.push({name: "Lq", value: redondear(Lq, 6)});
+    results.push({name: "Ls", value: redondear(Ls, 6)});
+
+    distribution = [];
+    let Fn = 0;
+    for(let n = 0; n <= k; n++){
+        let Pn = 0;
+        if(n<=C){
+            Pn = rho**n/factorial(n)*P0;
+        }
+        else
+        {
+            Pn = (rho**n*P0)/(C**(n-C)*factorial(C));
+        }
+        let PnRounded = redondear(Pn, 6); // Guardar el valor redondeado de Pn
+        Fn += Pn;
+        let FnRounded = redondear(Fn, 6); // Guardar el valor redondeado de Fn
+
+        distribution.push({n: n, Pn: PnRounded, Fn: FnRounded});
+    }
+    results.push({name: "distribution", value: distribution});
+    return results;
+}
+
+console.log(JSON.stringify(getColaNoLimiteNServidor(0.25, 0.1, 3, 60), null, 4));
